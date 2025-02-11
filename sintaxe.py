e19d3f961951d6db9a0c819bcdc3f79d2ec89059
angelo290894 <angelofabio.a@gmail.com>
2025-02-11 20:41:34 +0000

    meu primeiro commit

---
+++ sintaxe.py
@@ -0,0 +1,12 @@
+nome = "Angelo"
+sobrenome = "Job"
+nome_completo = "Angelo Job"
+#print("Seu nome completo eh:", nome_completo)
+#print("E a rimeira letra do nome:", nome[0])
+
+#print("quantidade de 'a': " ,  nome_completo.lower().count("a"))
+#print("asasas:", nome_completo.lower().replace("a", "G"))
+
+#print("Seu noem separado por tracos fica: ", " ".join(nome))
+print("seu nome separado", nome_completo.split())
+print("tem ' An' no seu nome?", "an" in nome_completo.lower() )
